f7f168f9e6d3110583615b68c29fa8f3aa6f7036
Bart Klomp <bart.klomp@ordina.nl>
2024-08-05 15:34:59 +0200

    Added angular environments

--- OrdinaMTech.Cv.Angular/src/app/services/cv.service.ts
+++ OrdinaMTech.Cv.Angular/src/app/services/cv.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Inject, inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Cv } from '../../models/cv.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class CvService {
   constructor(private httpClient:HttpClient) { }
 
   public GetCv() : Observable<Cv>{
-    return this.httpClient.get<Cv>("http://localhost:5000/cv");
+    return this.httpClient.get<Cv>(`${environment.url}/cv`);
   }
 
   public UploadFile(file:File) : Observable<string>{
@@ -19,6 +20,6 @@ export class CvService {
 
     formData.append("file", file);
 
-    return this.httpClient.post<string>("http://localhost:5000/cv/personalia/foto/upload", formData);
+    return this.httpClient.post<string>(`${environment.url}/cv/personalia/foto/upload`, formData);
   }
 }
