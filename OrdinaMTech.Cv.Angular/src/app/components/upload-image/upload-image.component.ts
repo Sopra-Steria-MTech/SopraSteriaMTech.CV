55e27874a8265beae78bfc0272f28c989cccc35b
Bart Klomp <bart.klomp@ordina.nl>
2024-08-23 11:30:38 +0200

    Removed commented code

--- OrdinaMTech.Cv.Angular/src/app/components/upload-image/upload-image.component.ts
+++ OrdinaMTech.Cv.Angular/src/app/components/upload-image/upload-image.component.ts
@@ -27,35 +27,5 @@ export class UploadImageComponent {
     this.cvService.UploadFile(element.files[0]).subscribe(result =>{
       this.foto = result;
     })
-    
-
-    /**
-     *  var bytes = new byte[0];
-        var file = files.FirstOrDefault();
-        if (file != null)
-        {
-            status = "Uploading...";
-            var fileStreamContent = new StreamContent(file.Data);
-            bytes = await fileStreamContent.ReadAsByteArrayAsync();
-            using (var formData = new MultipartFormDataContent())
-            {
-                // Add the HttpContent objects to the form data
-                formData.Add(fileStreamContent, "file", file.Name);
-
-                var response = await apiService.UploadFotoAsync(formData);
-                if (response.StatusCode == System.Net.HttpStatusCode.OK)
-                {
-                    status = string.Empty;
-                    using var stream = await response.Content.ReadAsStreamAsync();
-                    var jsonReader = new StreamReader(stream).ReadToEnd();
-                    foto = JsonConvert.DeserializeObject<byte[]>(jsonReader);
-                }
-                else
-                {
-                    status = await response.Content.ReadAsStringAsync();
-                }
-            }
-        }
-     */
   }
 }
